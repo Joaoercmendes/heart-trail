596cd0a8d58b1be7009c3169034aa63aa0c3be8c
joaoercmendes <joaomendesfullstack@gmail.com>
2025-03-13 00:19:43 +0000

    Done the project alone consulting the previous one when stuck

--- index2.js
+++ index2.js
@@ -1,25 +1,21 @@
 const bodyElement = document.querySelector('body');
 
 bodyElement.addEventListener('mousemove', (event) => {
-  //verifica posição do rato
-  const xPos = event.offsetX;
-  const yPos = event.offsetY;
+  const posX = event.offsetX;
+  const posY = event.offsetY;
 
-  //define o elemento e a posição
   const spanElement = document.createElement('span');
-  spanElement.style.left = xPos + 'px';
-  spanElement.style.top = yPos + 'px';
+  spanElement.style.left = posX + 'px';
+  spanElement.style.top = posY + 'px';
 
-  //coloca a span no body
   bodyElement.appendChild(spanElement);
 
-  //randomizar tamanho
   const size = Math.random() * 100;
+  spanElement.style.height = size + 'px';
   spanElement.style.width = size + 'px';
-  spanElement.style.height = size +'px';
 
-  //remover span depois de 2s
   setTimeout(() => {
     spanElement.remove();
   }, 2000);
+
 });
